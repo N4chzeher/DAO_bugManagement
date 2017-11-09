3b21c87c6699d98aad904f3761183b99e369125e
n4chzeher <diegorrojas@hotmail.com>
2017-11-09 08:38:21 +0100

    Create a text file that imulates a email that contains related information about a bug code given as parameter

---
+++ src/control/Main.java
@@ -0,0 +1,50 @@
+package control;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+/**
+ * Created by NACHZEHER on 02/11/2017.
+ */
+public class Main {
+    static final String bugsInfo = "rcBugPackage.txt";
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        ArrayList<String> paquetesAfectados = new ArrayList<String>();
+        System.out.println("Por favor ingrese un codigo de error:");
+        String bc = sc.nextLine();   // variable de codigo error
+
+        FileReader file1read = null;
+        BufferedReader bufFile1read = null;
+        String part2 = "";
+        String part1 = "";
+
+        try {
+            file1read = new FileReader(bugsInfo); // leer archivo
+            bufFile1read = new BufferedReader(file1read);  // Hcaer bufer de lo leido
+            String linea = "";             // String donde se guardara el contenido de la linea actual
+
+            while ((linea = bufFile1read.readLine()) != null) {      // Mientras haya una linea que leer
+                if (linea.contains(bc)) {                   // Si el String linea contiene la variable de codigo error
+                    String[] parts = linea.split(";");    // Se usa Split para partir el array donde encuentre un ;
+                    part1 = parts[0];               // Crea 2 subsString con cada parte resultante del Split
+                    part2 = parts[1];
+                    paquetesAfectados.add(part2);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        ArrayList<String> maintainers = Servicio.SearchMaintainerInfo(part2);
+        Servicio.sendEmail(Servicio.writeEmail(part1, maintainers));
+    }
+
+
+}
+
+
---
+++ src/control/Servicio.java
@@ -0,0 +1,105 @@
+package control;
+
+import java.io.*;
+import java.util.ArrayList;
+
+/**
+ * Created by NACHZEHER on 09/11/2017.
+ */
+public class Servicio {
+
+
+
+    public static void sendEmail(String form) {
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new StringReader(form));
+
+            File mail = new File("emailMaintainer.txt");
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(mail));
+            String linea = bufferedReader.readLine();
+
+            while (linea != null) {
+
+                bufferedWriter.write(linea);
+                linea = bufferedReader.readLine();
+
+                if (linea != null) {
+                    bufferedWriter.newLine();
+                }
+            }
+
+            bufferedWriter.flush();
+            bufferedWriter.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static ArrayList SearchMaintainerInfo(String code) {
+        String packInfo = "packageMaintainer.txt";
+        ArrayList<String> partes = new ArrayList<String>();
+        ArrayList<String> emailData = new ArrayList<>();
+        FileReader file2read = null;
+        BufferedReader bufFile2read = null;
+        String linea2 = "";
+        try {
+            file2read = new FileReader(packInfo);
+            bufFile2read = new BufferedReader(file2read);
+
+            while ((linea2 = bufFile2read.readLine()) != null) {
+                String parts[] = linea2.split(";");
+
+                if (code.equals(parts[0])) {
+
+                    emailData.add(parts[0]);
+                    emailData.add(parts[1]);
+                    emailData.add(parts[2]);
+                    break;
+                }
+
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (emailData.size() == 0) {
+            emailData.add("Error");
+            return emailData;
+        }
+
+        return emailData;
+
+    }
+    public static String writeEmail(String code, ArrayList emailData) {
+
+        ArrayList datos = emailData;
+        String formato;
+        if (datos.size() == 3) {
+
+            formato = "From: owner@bugs.debian.org" +
+                    System.lineSeparator() +
+                    "To:" + datos.get(2).toString() +
+                    System.lineSeparator() +
+                    "Dear:" + datos.get(1).toString() +
+                    System.lineSeparator() +
+                    "You have a new bug:" +
+                    System.lineSeparator() +
+                    datos.get(0).toString() + " -RC bug number #=" + code +
+                    System.lineSeparator() +
+                    "Please, fix it as son as possible." +
+                    System.lineSeparator() +
+                    "Cheers";
+
+        }else {
+            formato = "Error: No se puede enviar Email \n" +
+                    "\n" +
+                    "No se ha encontrado informacion relacionada con el package ";
+        }
+
+        return formato;
+    }
+
+}
